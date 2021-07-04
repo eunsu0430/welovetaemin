ccc1902afe4ad62cc2e1941b7c79163122410268
dnwldus410 <69383333+dnwldus410@users.noreply.github.com>
2021-07-04 23:03:00 +0900

    menu

--- exCap0627/Assets/Scripts/BtnManage.cs
+++ exCap0627/Assets/Scripts/BtnManage.cs
@@ -12,11 +12,13 @@ public class BtnManage : MonoBehaviour
     private bool pause = false;
   //  public AudioSource btnclickSound;
     private float fixedDeltatime;
+    private StartManage startmg;
 
     private void Awake()
     {
         {
             this.fixedDeltatime = Time.fixedDeltaTime;
+            startmg = FindObjectOfType<StartManage>();
         }
     }
     void Update()
@@ -64,9 +66,22 @@ public class BtnManage : MonoBehaviour
     {
        // btnclickSound.Play();
         pause = true;
-
-
-       
-     //   Time.fixedDeltaTime = this.fixedDeltatime * Time.timeScale;
+        
+        //   Time.fixedDeltaTime = this.fixedDeltatime * Time.timeScale;
+    }
+    public void developerBtn()
+    {
+        startmg.developclick = false;
+        startmg.developer();
+    }
+    public void exitClickBtn()
+    {
+        startmg.exitclick = false;
+        startmg.exitapp();
+    }
+    public void startmgBtn()
+    {
+        startmg.developclick = true;
+        startmg.exitclick = true;
     }
 }
